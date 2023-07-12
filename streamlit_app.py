9afae64ba1f429a75ff2b34a27a3aa5a54588f9f
Sara Cooper <37453834+sncoop@users.noreply.github.com>
2023-07-12 11:18:08 -0700

    Update streamlit_app.py
    added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+sreamlit.text('Omega 3 & Blueberry Oatmeal')
+sreamlit.text('Kale, Spinach & Rocket Smoothie')
+sreamlit.text('Hard-Boiled Free-Range Egg')
